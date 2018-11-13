01fcedc6f22a0869f0eb612fa2d00b47aa99ecf4
Igor Mukhin <igor.mukhin@gmail.com>
2018-11-13 18:02:33 +0200

    Added: Plugin configuration definition

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Setono\SyliusSchedulerPlugin\DependencyInjection;
 
+use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -16,7 +18,19 @@ final class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
 
+        /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->root('setono_sylius_scheduler');
+
+        $rootNodeChildren = $rootNode->children();
+        $rootNodeChildren->scalarNode('driver')->defaultValue(SyliusResourceBundle::DRIVER_DOCTRINE_ORM);
+
+        // Wipe logs in X days after command execution
+        // Specify 0 to never wipe logs
+        $rootNodeChildren->scalarNode('wipe_logs_in')->defaultValue(0);
+
+        // We can specify emails to receive error reports on every Job
+        // But here we can specify emails which receive error reports for all Jobs
+        $rootNodeChildren->scalarNode('error_reports_emails')->defaultValue(0);
 
         return $treeBuilder;
     }
--- src/SetonoSyliusSchedulerPlugin.php
+++ src/SetonoSyliusSchedulerPlugin.php
@@ -1,13 +1,26 @@
 <?php
 
+/** @noinspection TraitsPropertiesConflictsInspection */
+
 declare(strict_types=1);
 
 namespace Setono\SyliusSchedulerPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Sylius\Bundle\ResourceBundle\AbstractResourceBundle;
+use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 
-final class SetonoSyliusSchedulerPlugin extends Bundle
+final class SetonoSyliusSchedulerPlugin extends AbstractResourceBundle
 {
     use SyliusPluginTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSupportedDrivers(): array
+    {
+        return [
+            SyliusResourceBundle::DRIVER_DOCTRINE_ORM,
+        ];
+    }
 }
